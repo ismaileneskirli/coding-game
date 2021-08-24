a8f249a24352399c962fbb39d4499923f1250800
ismaileneskirli <69651222+ismaileneskirli@users.noreply.github.com>
2021-08-25 00:38:04 +0300

    front-end moved to a new repo

--- front-end/src/services/quizService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default class QuizService {
-  getRandomQuiz() {
-    return axios.get("https://quizapi.io/api/v1/questions", {
-      params: { apiKey: "qdEzmI1siD96pLSOfUQ6PlDvZ8RNUeelHdLTWzf7" },
-    });
-  }
-}
